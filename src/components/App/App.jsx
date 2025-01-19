86ce99bc1c7feb3f5a2ca885adf228aad74ebdcf
Yuliia Diachenko <zirkanew82@gmail.com>
2025-01-19 21:00:42 +0200

    add routing & fix for deploy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,6 @@ import Layout from "../Layout/Layout";
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
 const UsersPage = lazy(() => import("../../pages/UsersPages/UsersPage"));
 const BookingPage = lazy(() => import("../../pages/BookingPage/BookingPage"));
-import css from "./App.module.css";
 
 export default function App() {
 
